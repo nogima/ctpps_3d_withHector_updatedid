4de51b81f655403a4dfbf44a3d22c5ae70bb3fcb
Helio <nogima@cern.ch>
2017-05-15 03:29:58 +0200

    round electrons to adc conversion

--- SimCTPPS/CTPPSPixelDigiProducer/src/RPixDummyROCSimulator.cc
+++ SimCTPPS/CTPPSPixelDigiProducer/src/RPixDummyROCSimulator.cc
@@ -62,7 +62,7 @@ void RPixDummyROCSimulator::ConvertChargeToHits(const std::map<unsigned short, d
 
       int col = pixel_no / 160;
       int row = pixel_no % 160;
-      int adc = int(i->second / electron_per_adc_ + 0.5);
+      int adc = int(round(i->second / electron_per_adc_));
 
 // std::cout << "RPixDummyROCSimulator: col = " << col << "  row = " << row << "  adc = " << adc << "  electrons = " << i->second << endl; 
 
